36da9f7b975f9a0b56bfbeae53a8fcd0bb86d88b
Zeck Li <11781254+zeckli@users.noreply.github.com>
2025-04-12 23:08:38 +0800

    install GA

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,15 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-11Z2GSPJBK"></script>
+        <script async dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-11Z2GSPJBK');
+          `,
+        }}/>
         <Links />
       </head>
       <body className="grid center">
